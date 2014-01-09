2ad0875cbf8558ed28447f91df9e2d37cbfb5e5d
pooja_joshi1 <pooja_joshi1@persistent.co.in>
2014-01-09 11:10:57 +0530

    Recent update in code

--- workspace/git/src/test/java/com/psl/selenium/pooja_maven/TestingGoogle.java
+++ workspace/git/src/test/java/com/psl/selenium/pooja_maven/TestingGoogle.java
@@ -1,63 +1,86 @@
 package com.psl.selenium.pooja_maven;
 
 
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-
+import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class TestingGoogle {     
-                  private ScreenshotHelper screenshotHelper;
-                  private static  WebDriver driver=new FirefoxDriver();
-                                private static  String baseUrl;
-                  
-                                
-        @Before
-         public void openBrowser() {
-                  	baseUrl = "http://www.wordpress.com/";
-                    driver.get(baseUrl);
-                    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-                    screenshotHelper = new ScreenshotHelper();
-                  }
-                  
-        @After
-         public void saveScreenshotAndCloseBrowser() throws IOException {
-                    screenshotHelper.saveScreenshot("screenshot.png");
-                    driver.quit();
-                  }
-                  
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class TestingGoogle {
+      // private String baseUrl;
+      //  private WebDriver driver;
+      
+        private ScreenshotHelper screenshotHelper;
+        private static  WebDriver driver;
+            private static  String baseUrl;
+        
+            
+            @Before
+        public void openBrowser() {
+                  driver=new FirefoxDriver();
+                  baseUrl = "http://www.bing.com/";
+                  driver.get(baseUrl);
+                  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+          screenshotHelper = new ScreenshotHelper();
+        }
+        
+         
         @Test
-         public void Valid()throws Exception{
-          			driver.findElement(By.name("log")).sendKeys("pooja_joshi1@persistent.co.in");
-          			driver.findElement(By.name("pwd")).sendKeys("!hero5bl");
-          			driver.findElement(By.xpath(".//*[@id='signin-form']/input[4]")).click();
-          			Boolean Element=driver.findElement(By.xpath(".//*[@id='reader-content']/div[1]/h2")).isDisplayed();
-          			assertTrue("Blogs I Follow",Element);
-          		}
-                  
-                
-                  
-                  private class ScreenshotHelper {
-                      public void saveScreenshot(String screenshotFileName) throws IOException {
-                      File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-                      FileUtils.copyFile(screenshot, new File(screenshotFileName));
-                    }
-                  }
-                                
-                                
-                
+        public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException {
+      // assertEquals("The page title should equal Google at the start of the test.", "Google", driver.getTitle());
+          WebElement searchField = driver.findElement(By.id("sb_form_q"));
+          searchField.sendKeys("Drupal");
+          searchField.submit();
+          String verify=driver.findElement(By.xpath("//*[@id='wg0']/li[1]/div/div/div[1]/h3/a/strong[1]")).getText();
+          assertEquals("Drupal",verify);
+        
+        }
+        @Test
+        public void pageTitleAfterSearchShouldBeginWithDrupal1() throws IOException {
+      // assertEquals("The page title should equal Google at the start of the test.", "Google", driver.getTitle());
+          WebElement searchField = driver.findElement(By.id("sb_form_q"));
+          searchField.sendKeys("Selenium");
+          searchField.submit();
+          String verify=driver.findElement(By.xpath("//*[@id='wg0']/li[1]/div/div/div[1]/h3/a/strong[1]")).getText();
+          assertEquals("Selenium",verify);
+        
+        }
+      
+
+            @After
+              public void saveScreenshotAndCloseBrowser() throws IOException {
+                screenshotHelper.saveScreenshot("screenshot.png");
+                driver.quit();
+              }
+            
+        
+        private class ScreenshotHelper {
+            public void saveScreenshot(String screenshotFileName) throws IOException {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File(screenshotFileName));
+          }
+        }
+            
+            
+      
 }
