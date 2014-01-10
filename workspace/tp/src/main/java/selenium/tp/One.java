f282ac42b03d9a5e7c219ffb4f92d90f09a8fee1
pooja_joshi1 <pooja_joshi1@persistent.co.in>
2014-01-10 12:18:58 +0530

    Sonar

---
+++ workspace/tp/src/main/java/selenium/tp/One.java
@@ -0,0 +1,17 @@
+package selenium.tp;
+public class One {
+  String message = "foo";
+  String message2 = "toto";
+
+  public String foo() {
+    return message;
+  }
+
+  public String toto() {
+    return message2;
+  }
+
+  public void uncoveredMethod() {
+    System.out.println(foo());
+  }
+}
